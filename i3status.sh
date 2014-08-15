ad0be7287759b77de4a63affd98b07d260bb7598
Edwin Flores <edwin@flores.geek.nz>
2014-08-15 15:27:33 +1200

    Update westpac balance widget to new JSON-based i3status format

--- i3status.sh
+++ i3status.sh
@@ -2,8 +2,9 @@
 
 i3status | while read line
 do
-  if [ -e ~/.westpac-balance ]; then
-    line=`cat ~/.westpac-balance <(echo ' | '$line)`
+  if [ -e ~/.westpac-balance ] && [[ $line =~ full_text ]]; then
+    balance=`cat ~/.westpac-balance`
+    line=`echo $line | sed "s/{/{\"full_text\":\"$balance\"},{/"`
   fi
   echo $line
 done
